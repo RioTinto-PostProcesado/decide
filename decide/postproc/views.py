86908a004ca63dd7866b1469012ca9b9944f9b62
ferlopmor1 <74310075+ferlopmor1@users.noreply.github.com>
2021-01-03 15:25:50 +0100

    Feature: implementada ley d'Hondt #7
    Se ha implementado la ley d'Hondt en las votaciones.

--- decide/postproc/views.py
+++ decide/postproc/views.py
@@ -16,6 +16,21 @@ class PostProcView(APIView):
         out.sort(key=lambda x: -x['postproc'])
         return Response(out)
 
+
+    def dhont(self, options, escanio):
+        #Añadimos escaños para todas las opciones
+        for opt in options:
+            opt['escanio'] = 0
+
+        #Para cada escaño se hacen los calculos para averiguar que opción tiene el mayor cociente, el que tenga el mayor, se lleva el escaño
+        for i in range(escanio):
+            max(options, 
+                key = lambda x : x['votes'] / (x['escanio'] + 1.0))['escanio'] += 1
+
+        #Se ordenan las opciones según los escaños
+        options.sort(key=lambda x: -x['escanio'])
+        return Response(options)
+
     def post(self, request):
         """
          * type: IDENTITY | EQUALITY | WEIGHT
@@ -32,7 +47,13 @@ class PostProcView(APIView):
         t = request.data.get('type', 'IDENTITY')
         opts = request.data.get('options', [])
 
+
         if t == 'IDENTITY':
             return self.identity(opts)
+
+        if t == 'DHONDT':
+            escanio = int(float(request.data.get('escanio', '5')))
+            return self.dhont(opts, escanio)
+
 
         return Response({})
