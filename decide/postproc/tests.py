6d66b3d089cab5f7133ad840165440a910165e3f
alegomcab1 <alegomcab1@gmail.com>
2021-01-12 18:56:06 +0100

    Test Ampliacion de test para aumentar coverage

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -211,3 +211,51 @@ class PostProcTestCase(APITestCase):
 
         values = response.json()
         self.assertEqual(values, expected_result)
+
+
+    def testParidad2(self):
+        """
+            * Definicion: Test positivo con solo un candidato de todos los posibles
+            * Entrada: Votacion (Json)
+                - Option: Nombre del partido
+                - Number: Id de la opcion
+                - Votes: Numero de votos de esa votacion
+                - PostProc: Numero de personas que van a ir en la lista una vez aplicada la paridad
+                - Candidatos: Sexo e ID de los candidatos
+            * Salida: Codigo 200 y json de la paridad
+        """
+        data = {
+            'type': 'PARIDAD',
+            'options': [
+                { 'option': 'Partido Unico', 'number': 1, 'votes': 5 , 'postproc': 5, 'candidatos': [
+                 {'sexo':'hombre','id':'1'}
+                ,{'sexo':'hombre','id':'3'}
+                ,{'sexo':'mujer','id':'2'}
+                ,{'sexo':'mujer','id':'4'}
+                ,{'sexo':'mujer','id':'5'}
+                ]}
+            ]
+        }
+
+        expected_result = [
+            { 'option': 'Partido Unico', 'number': 1, 'votes': 5, 'postproc': 5, 'candidatos': [
+                 {'sexo':'hombre','id':'1'}
+                ,{'sexo':'hombre','id':'3'}
+                ,{'sexo':'mujer','id':'2'}
+                ,{'sexo':'mujer','id':'4'}
+                ,{'sexo':'mujer','id':'5'}
+                ],
+                'paridad': [
+                 {'sexo':'mujer','id':'2'}
+                ,{'sexo':'hombre','id':'1'}
+                ,{'sexo':'mujer','id':'4'}
+                ,{'sexo':'hombre','id':'3'}
+                ,{'sexo':'mujer','id':'5'}
+                ]}
+        ]
+        
+        response = self.client.post('/postproc/', data, format='json')
+        self.assertEqual(response.status_code, 200)
+
+        values = response.json()
+        self.assertEqual(values, expected_result)
