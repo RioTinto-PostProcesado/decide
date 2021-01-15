94b037ce6ec3ba08cf86f797ecddce19b389caee
antmorgon4 <antmorgon4@alum.us.es>
2021-01-15 22:35:52 +0100

    FIX: Arreglado incidencia en tests.py; #46

--- decide/postproc/tests.py
+++ decide/postproc/tests.py
@@ -461,36 +461,7 @@ class PostProcTestCase(APITestCase):
         self.assertEqual(values, expected_result)
 
 
-    def test_SainteLague1(self):
-        data = {
-            'type': 'SAINTE',
-            'seats': 12,
-            'options': [
-                {'option': 'Partido 1', 'number': 1, 'votes': 50},
-                {'option': 'Partido 2', 'number': 2, 'votes': 10},
-                {'option': 'Partido 3', 'number': 3, 'votes': 34},
-                {'option': 'Partido 4', 'number': 4, 'votes': 25},
-                {'option': 'Partido 5', 'number': 5, 'votes': 56},
-                {'option': 'Partido 6', 'number': 6, 'votes': 170},
-            ]
-        }
-
-        expected_result = [
-            {'option': 'Partido 6', 'number': 6, 'votes': 170, 'postproc': 9},
-            {'option': 'Partido 5', 'number': 5, 'votes': 56, 'postproc': 2},
-            {'option': 'Partido 1', 'number': 1, 'votes': 50, 'postproc': 1},
-            {'option': 'Partido 3', 'number': 3, 'votes': 34, 'postproc': 0},
-            {'option': 'Partido 4', 'number': 4, 'votes': 25, 'postproc': 0},
-            {'option': 'Partido 2', 'number': 2, 'votes': 10, 'postproc': 0},
-        ]
-
-        response = self.client.post('/postproc/', data, format='json')
-        self.assertEqual(response.status_code, 200)
-
-        values = response.json()
-        self.assertEqual(values, expected_result)
-
-
+    
     def test_saint_lague_sin_paridad(self):
         
         """
